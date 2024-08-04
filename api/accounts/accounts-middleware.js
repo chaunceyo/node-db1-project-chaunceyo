cc9e797ae0d50194bdb12ed839dc9e43745fc707
Chaunceyo <chaunceyogletree725@gmail.com>
2024-08-04 12:57:04 -0400

    finished

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -2,16 +2,15 @@ const Account = require("./accounts-model")
 
 exports.checkAccountPayload = (req, res, next) => {
   // DO YOUR MAGIC
-  console.log("checkAccountPayload middleware")
   const error = {status: 400}
   const {name, budget} = req.body
-  if(!name || !budget){
+   if(name === undefined || budget === undefined){
     error.message = "name and budget are required" 
+  }else if(typeof budget !== 'number' || isNaN(budget)){
+    error.message = "budget of account must be a number"
   }else if(name.trim().length < 3 || name.trim().length > 100){
       error.message = "name of account must be between 3 and 100"
-    }else if(typeof budget !== 'number' || isNaN(budget)){
-      error.message = "budget of account must be a number"
-        }else if(budget > 1000000 || budget < 0){
+    }else if(budget > 1000000 || budget < 0){
           error.message = "budget of account is too large or too small"
         }
   
